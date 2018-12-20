4f8b63111103c6e47bf6f56478ebdaf4a2250120
Michal Stawarski <m.ski@poczta.fm>
2018-12-20 14:24:48 +0100

    Version-1.0

---
+++ src/test/java/pl/codecity/module/users/ModuleUsersApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.codecity.module.users;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModuleUsersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
